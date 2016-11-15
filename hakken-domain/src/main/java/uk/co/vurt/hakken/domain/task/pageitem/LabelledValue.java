bbe942807c3a3aa9021eae2c75628899adf19dd9
chriskeep <chris.keep@wmfs.net>
2016-11-15 12:57:06 +0000

    Added attribute singleSelect to LabelledValue class to enable validation for multi-select lists to include values that can only be selected on their own.

--- hakken-domain/src/main/java/uk/co/vurt/hakken/domain/task/pageitem/LabelledValue.java
+++ hakken-domain/src/main/java/uk/co/vurt/hakken/domain/task/pageitem/LabelledValue.java
@@ -4,6 +4,7 @@ public class LabelledValue {
 
 	String label;
 	String value;
+	Boolean singleSelect;
 	
 	public String getLabel() {
 		return label;
@@ -20,7 +21,16 @@ public class LabelledValue {
 	public void setValue(String value) {
 		this.value = value;
 	}
+	
+	public Boolean getSingleSelect() {
+		return singleSelect;
+	}
 
+	public void setSingleSelect(Boolean singleSelect) {
+		this.singleSelect = singleSelect;
+	}
+
+	
 	@Override
 	public String toString() {
 		return "LabelledValue [label=" + label + ", value=" + value + "]";
