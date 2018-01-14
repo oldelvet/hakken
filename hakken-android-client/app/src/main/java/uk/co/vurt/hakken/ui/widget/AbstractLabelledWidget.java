e8cf1f634ef035e7de039d1ab047a67372530dc9
Richard Mortimer <richm@oldelvet.org.uk>
2018-01-14 23:55:22 +0000

    Extend AbstractLabelledWidget with constructors required for inflation from layouts

--- hakken-android-client/app/src/main/java/uk/co/vurt/hakken/ui/widget/AbstractLabelledWidget.java
+++ hakken-android-client/app/src/main/java/uk/co/vurt/hakken/ui/widget/AbstractLabelledWidget.java
@@ -1,6 +1,10 @@
 package uk.co.vurt.hakken.ui.widget;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -10,6 +14,26 @@ public abstract class AbstractLabelledWidget extends LinearLayout {
 	
 	public AbstractLabelledWidget(Context context) {
 		super(context);
+		doCommonSetup();
+	}
+
+	public AbstractLabelledWidget(Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+		doCommonSetup();
+	}
+
+	public AbstractLabelledWidget(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		doCommonSetup();
+	}
+
+	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+	public AbstractLabelledWidget(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		doCommonSetup();
+	}
+
+	private void doCommonSetup() {
 		this.setOrientation(VERTICAL);
 	}
 	
@@ -18,5 +42,4 @@ public abstract class AbstractLabelledWidget extends LinearLayout {
 			label.setText(labelText);
 		}
 	}
-	
 }
